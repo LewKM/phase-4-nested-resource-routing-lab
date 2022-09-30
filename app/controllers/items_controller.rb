9e2e60ff7e86b3ffd5b710b98a78b9a31b944350
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-09-30 14:50:24 +0300

    commit tree b3ac3056aee8fd485251e7fb73882d2a35c7beed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,25 +12,25 @@ class ItemsController < ApplicationController
     render json: items, include: :user
   end
 
-  def show
-    item = find_item
-    render json: item
-  end
-
   def create
     user = find_user
     item = user.items.create(item_params)
     render json: item, status: :created
   end
 
-  private
-
-  def find_item
-    Item.find(params[:id])
+  def show
+    item = find_item
+    render json: item, include: :user
   end
+
+  private
 
   def find_user
     User.find(params[:user_id])
+  end
+
+  def find_item
+    Item.find(params[:id])
   end
 
   def item_params
